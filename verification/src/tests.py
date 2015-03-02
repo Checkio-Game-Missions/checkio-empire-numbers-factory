836a855cfeeb773de7cccab9a4c1811d8b5a1a91
Valentin Bryukhanov <bvv.mag@gmail.com>
2015-03-02 16:35:49 +0300

    referee tests

---
+++ verification/src/tests.py
@@ -0,0 +1,59 @@
+TESTS = {
+    "Level_1": [
+        {"input": 20, "answer": 45},
+
+        {"input": 21, "answer": 37},
+
+        {"input": 17, "answer": 0},
+
+        {"input": 33, "answer": 0},
+
+
+        {"input": 125, "answer": 555},
+
+        {"input": 560, "answer": 2578},
+
+        {"input": 8146, "answer": 0},
+
+        {"input": 3247, "answer": 0},
+
+        {"input": 955, "answer": 0},
+
+        {"input": 124, "answer": 0},
+
+        {"input": 1680, "answer": 5678},
+
+
+        {"input": 3645, "answer": 5999},
+
+        {"input": 35, "answer": 57},
+
+        {"input": 42, "answer": 67},
+
+        {"input": 3125, "answer": 55555},
+        {"input": 1024, "answer": 2888},
+        {"input": 6561, "answer": 9999},
+        {"input": 686, "answer": 2777},
+
+        {"input": 9973, "answer": 0},
+        {"input": 1536, "answer": 3888},
+
+        {"input": 7334, "answer": 0},
+
+        {"input": 9545, "answer": 0},
+
+        {"input": 1715, "answer": 5777},
+
+        {"input": 3645, "answer": 5999},
+        {"input": 3275, "answer": 0},
+        {"input": 2310, "answer": 0},
+
+        {"input": 288, "answer": 489},
+
+        {"input": 1800, "answer": 5589},
+
+        {"input": 9999, "answer": 0},
+        {"input": 9000, "answer": 55589},
+        {"input": 12, "answer": 26}
+    ]
+}
